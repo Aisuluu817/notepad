2e0e940e66c847f4dd2976fd9e688ecf019c9c78
Aisuluu817 <a.arstanbaeva01@gmail.com>
2020-12-23 14:40:50 +0600

    Initial commit

---
+++ WriteAndRead.java
@@ -0,0 +1,34 @@
+import java.io.*;
+public class WriteAndRead {
+
+    public static String readFromFile(String fileName) {
+        String lines = "";
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                lines = lines + line + "\n";
+            }
+
+            bufferedReader.close();
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+
+        }
+
+        return lines;
+    }
+
+    public static void writeToFile(String filename, String lines) {
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filename));
+            bufferedWriter.write(lines);
+            bufferedWriter.close();
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+    }
+
+}
